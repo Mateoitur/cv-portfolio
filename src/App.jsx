58dd3ac74f235be28beb4b6d6fd5cecedcfc407e
Mateo <s225095@dtu.dk>
2025-09-18 21:06:28 +0200

    More changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect } from 'react'
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
 
 import Header from './components/Header'
 import About from './components/About'
@@ -8,7 +12,7 @@ import Contact from './components/Contact'
 const App = () => {
 
   return (
-    <div className='w-full overflow-hidden scroll-smooth'>
+    <div className='w-full overflow-x-hidden'>
       <Header />
       <About />
       <Portfolio />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { motion } from 'framer-motion'
-import Navbar from './Navbar'
 
 const Header = () => {
   return (
     <div className='hero' id='Home'>
       <h1 className='heading'>MATEO ITURMENDI</h1>
-      <motion.img transition={{ease: "easeInOut"}} whileHover={{ scale: 1.02 }} className='avatar' src="./headshot.JPG" alt="" />
+      <img className='avatar' src="./headshot.JPG" alt="" />
       <h2 className='subheading'>LEARNING TO DESIGN, INNOVATE & CREATE</h2>
     </div>
   )
