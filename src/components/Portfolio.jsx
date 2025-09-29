0e46ddad16dee4839f97a3b6621157d12b84888e
Mateo <s225095@dtu.dk>
2025-09-29 16:24:05 +0200

    Cleared up sections

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,24 +1,12 @@
 import React, { useMemo } from "react"
 
-const images = import.meta.glob("/src/assets/gallery/*.{png,jpg,jpeg,webp}", {
-  eager: true,
-  as: "url",
-});
-
 const Portfolio = () => {
-    const urls = useMemo(() => Object.values(images).sort(), []);
-
   return (
-    <div className="section" id="More">
-      <h1 className="heading2 text">MORE WORK</h1>
-      <h2 className="text text-md text-onPrimary mt-1 mb-20">Scroll around to see more</h2>
-      <div className='w-full grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-25 max-w-6xl px-10'>
-        {urls.map((src, i) => (
-            <div key={i} className="gallery-row w-full aspect-square rounded-2xl overflow-hidden shadow-xl">
-              <img src={src} alt={`Gallery ${i+1}`} className="w-full h-full object-cover"/>
-            </div>
-          ))}
-      </div>
+    <div className='section' id='More'>
+      <h1 className='heading2 text'>MORE WORK</h1>
+      <h2 className='text text-md text-onPrimary mt-1 mb-20'>
+        Scroll around to see more
+      </h2>
     </div>
   )
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,25 +1,12 @@
 const Work = () => {
   return (
-    <div ref={root} className="section" id="Work">
-        <h1 className="heading2 ">MY WORK</h1>
-        <h2 className="text-md text-onPrimary mt-1 mb-10">Some of my creations</h2>
+    <div ref={root} className='section' id='Work'>
+      <h1 className='heading2 '>MY WORK</h1>
+      <h2 className='text-md text-onPrimary mt-1 mb-10'>
+        Some of my creations
+      </h2>
 
-        <div className="w-full relative flex overflow-x-auto gap-10 justify-center no-scrollbar">
-          <div className="flex-none">
-            <img className="img-carrousel" src="./work1.png" alt="" />
-            <button className="font-main text-onPrimary bg-secondary px-8 py-3 rounded-full transition-transform hover:scale-105 hover:shadow-2xl">ESTATE</button>
-          </div>
-          <div className="flex-none">
-            <img className="img-carrousel" src="./work1.png" alt="" />
-            <button className="font-main text-onPrimary bg-secondary px-8 py-3 rounded-full transition-transform hover:scale-105 hover:shadow-2xl">ESTATE</button>
-          </div>
-
-          <div className="flex-none">
-            <img className="img-carrousel" src="./work1.png" alt="" />
-            <button className="font-main text-onPrimary bg-secondary px-8 py-3 rounded-full transition-transform hover:scale-105 hover:shadow-2xl">ESTATE</button>
-          </div>
-        </div>
-        
+      <div className='w-full relative flex overflow-x-auto gap-10 justify-center no-scrollbar'></div>
     </div>
   )
 }
